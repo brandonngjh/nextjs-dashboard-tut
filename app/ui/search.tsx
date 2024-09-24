c54bd0e150952d1d27f15ed619eebc0e38e2def5
brandonngjh <brandonng2210@gmail.com>
2024-09-24 14:57:25 +0800

    separate onchangehandler

--- app/ui/search.tsx
+++ app/ui/search.tsx
@@ -16,6 +16,11 @@ export default function Search({ placeholder }: { placeholder: string }) {
     router.replace(`${pathname}?${params.toString()}`);
   }, 300);
 
+  const onChangeHandler = (e: React.ChangeEvent<HTMLInputElement>) => {
+    handleSearch(e.target.value);
+    return null;
+  };
+
   return (
     <div className="relative flex flex-1 flex-shrink-0">
       <label htmlFor="search" className="sr-only">
@@ -24,10 +29,7 @@ export default function Search({ placeholder }: { placeholder: string }) {
       <input
         className="peer block w-full rounded-md border border-gray-200 py-[9px] pl-10 text-sm outline-2 placeholder:text-gray-500"
         placeholder={placeholder}
-        onChange={(e) => {
-          handleSearch(e.target.value);
-          return null;
-        }}
+        onChange={onChangeHandler}
         defaultValue={searchParams.get("query")?.toString()}
       />
       <MagnifyingGlassIcon className="absolute left-3 top-1/2 h-[18px] w-[18px] -translate-y-1/2 text-gray-500 peer-focus:text-gray-900" />
