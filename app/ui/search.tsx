1749e3d37c9554ec19dd3cb62afcd3cec4553e9a
brandonngjh <brandonng2210@gmail.com>
2024-09-26 10:47:34 +0800

    fix eslint issue

--- app/ui/search.tsx
+++ app/ui/search.tsx
@@ -12,13 +12,14 @@ export default function Search({ placeholder }: { placeholder: string }) {
   const handleSearch = useDebouncedCallback((term: string) => {
     const params = new URLSearchParams(searchParams);
     params.set("page", "1");
-    term ? params.set("query", term) : params.delete("query");
+    if (term) {
+      params.set("query", term);
+    } else {
+      params.delete("query");
+    }
     router.replace(`${pathname}?${params.toString()}`);
   }, 300);
 
-  const onChangeHandler = (e: React.ChangeEvent<HTMLInputElement>) => {
-    handleSearch(e.target.value);
-  };
 
   return (
     <div className="relative flex flex-1 flex-shrink-0">
@@ -28,7 +29,9 @@ export default function Search({ placeholder }: { placeholder: string }) {
       <input
         className="peer block w-full rounded-md border border-gray-200 py-[9px] pl-10 text-sm outline-2 placeholder:text-gray-500"
         placeholder={placeholder}
-        onChange={onChangeHandler}
+        onChange={(e) => {
+          handleSearch(e.target.value);
+        }}
         defaultValue={searchParams.get("query")?.toString()}
       />
       <MagnifyingGlassIcon className="absolute left-3 top-1/2 h-[18px] w-[18px] -translate-y-1/2 text-gray-500 peer-focus:text-gray-900" />
